3062517d12c3afc37a9151cd3bf5f5f63fb86095
Sanchex-22 <David21.cs70@gmail.com>
2023-10-26 17:44:59 -0500

    arreglos con el form

--- laboratorio14/laboratorio14.1.php
+++ laboratorio14/laboratorio14.1.php
@@ -1,27 +1,29 @@
 <?php
-    session_start();
-    if(isset($_REQUEST['usuario']) && isset($_REQUEST['clave'])){
-        $usuario = $_REQUEST['usuario'];
-        $clave = $_REQUEST['clave'];
+session_start();
 
-        $salt = substr($usuario,0,2);
-        $clave_crypt = crypt($clave,$salt);
+if (isset($_REQUEST['usuario']) && isset($_REQUEST['clave'])) {
+    $usuario = $_REQUEST['usuario'];
+    $clave = $_REQUEST['clave'];
 
-        require_once("class/usuarios.php");
-        $obj_usuarios = new usuarios();
-        $usuario_validado = $obj_usuarios->validar_usuario($usuario,$clave_crypt);
+    $salt = substr($usuario, 0, 2);
+    $clave_crypt = crypt($clave, $salt);
 
-        foreach($usuario_validado as $array_resp){
-            foreach($array_resp as $value){
-                $nfilas = $value;
-            }
-        }
+    require_once("class/usuarios.php");
+    $obj_usuarios = new usuarios();
+    $usuario_validado = $obj_usuarios->validar_usuario($usuario, $clave_crypt);
 
-        if($nfilas > 0){
-            $usuario_validado = $usuario;
-            $_SESSION['usuario_valido']=$usuario_validado;
-        }
+    // Verificar si $usuario_validado es un array y si tiene elementos
+    if (is_array($usuario_validado) && count($usuario_validado) > 0) {
+        $nfilas = $usuario_validado[0]['COUNT(*)']; // Suponiendo que COUNT(*) sea el nombre de la columna
+    } else {
+        $nfilas = 0;
     }
+
+    if ($nfilas > 0) {
+        $usuario_validado = $usuario;
+        $_SESSION['usuario_valido'] = $usuario_validado;
+    }
+}
 
 ?>
 
@@ -43,24 +45,24 @@
         }
         // intento fallido
         else if (isset($usuario)){
-            print("");
-            print("");
-            print("");
+            print("<br><br>\n");
+            print("<p ALIGN='CENTER'>Acceso no Autorizado</p>\n");
+            print("<p ALIGN='CENTER'>[<a href='laboratorio14.1.php'>Conectar</a>]</p>\n");
         }
         // session no inciada
         else{
-            print("");
-            print("");
-            print("");
+            print("<br><br>\n");
+            print("<p class='parradocentrado'>Esta Zona tiene el acceso restringido<br>"."Para entrar debe identificarse</p>");
+            print("<form action='laboratorio14.1.php'>\n");
 
-            print("");
+            print("<p><label class='etiqueta-entrada'>Usuario</label></p>\n");
 
-            print("");
-            print("");
-            print("");
-            print("");
-            print("");
-            print("");
+            print("<input type='text' name='usuario' size='15'></p>\n");
+            print("<p><label class='etiqueta-entrada'>Contraseña</label></p>\n");
+            print("<input type='password' name='clave' size='15'></p>\n");
+            print("<input type='submit' value='entrar'></p>\n");
+            print("</form>\n");
+            print("<p class='parrafocentrado'>NOTA: si no dispone de la identificacion o tiene problemas"."para entrar <br> pongase en contacto con el"."<a href='MAILTO:webmaster@localhost'>Administrador</a>del sitio</p>");
 
             print("");
         }
--- laboratorio14/labsdb.sql
+++ laboratorio14/labsdb.sql
@@ -3,20 +3,22 @@ CREATE TABLE usuarios(
     id smallint(5) unsigned NOT NULL auto_increment,
     usuario varchar(20) NOT NULL default '',
     clave varchar(20) NOT NULL default '',
-    PRIMARY KEY (id);
+    PRIMARY KEY (id)
 );
 
 INSERT INTO usuarios VALUES(1,'testuser','teXB5LK3JWG6g');
 
-CREATE DEFINER='root'@'localhost' PROCEDURE 'sp_validar_usuario'(
+DELIMITER $$
+CREATE PROCEDURE sp_validar_usuario(
     IN param_user VARCHAR(255),
     IN param_pass VARCHAR(255)
 )
-BEGIN @s = CONCAT("SELECT count(*) FROM usuarios
-WHERE usuario='",param_user,"' AND clave ='",param_pass,"'");
-
+BEGIN
+    SET @s = CONCAT("SELECT COUNT(*) FROM usuarios WHERE usuario='", param_user, "' AND clave='", param_pass, "'");
+    
     PREPARE stmt FROM @s;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
-    
-END
+END;
+$$
+DELIMITER ;
